6a495186c871b15b840b9cb2e89ea0e1f7f42b0b
Ruslan Zyabbarov <rupam71@gmail.com>
2019-12-04 22:38:35 -0500

    Added UseEffect hooks

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import uuid from 'uuid/v1';
 import NewSongForm from '../Form';
 const SongList = () => {
@@ -7,10 +7,19 @@ const SongList = () => {
     { title: 'Two', id: 2 },
     { title: 'Three', id: 3 }
   ]);
+  const [age, setAge] = useState(23);
 
   const addSong = songName => {
     setSongs([...songs, { title: songName, id: uuid() }]);
   };
+
+  useEffect(() => {
+    console.log('rerenders');
+  }, [songs]);
+
+  useEffect(() => {
+    console.log('ageeegeg');
+  }, [age]);
   return (
     <div className="song-list">
       <ul>
@@ -20,6 +29,7 @@ const SongList = () => {
       </ul>
 
       <NewSongForm addSong={addSong} />
+      <button onClick={() => setAge(age + 1)}> CLICK </button>
     </div>
   );
 };
